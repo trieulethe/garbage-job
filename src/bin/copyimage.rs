480248596b91eb128e447afb8e04a4316e8b5631
trieule <amhanhnguxu@gmail.com>
2025-04-22 08:49:58 +0700

    update copy img

---
+++ src/bin/copyimage.rs
@@ -0,0 +1,77 @@
+use std::{error::Error, fs, io, path::Path};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let root_folder = "/opt/media/";
+    // let root_dir = Path::new(root_folder);
+    // println!()
+    let folders = read_folders_in_folder(root_folder)?;
+    println!("Folders in the specified folder:");
+    for folder in folders {
+        println!("{}", folder);
+        copy_and_rename_json_files(&folder)?;
+    }
+
+    Ok(())
+}
+
+fn read_folders_in_folder(folder_path: &str) -> io::Result<Vec<String>> {
+    let mut folders = Vec::new();
+
+    for entry in fs::read_dir(folder_path)? {
+        let entry = entry?;
+        println!("entry {:?}", entry.path());
+        let string = entry.path().display().to_string();
+        folders.push(string);
+    }
+
+    Ok(folders)
+}
+
+fn copy_and_rename_json_files(folder_path: &str) -> io::Result<()> {
+    let folder_path = Path::new(folder_path);
+
+    // Check if the folder exists
+    if !folder_path.is_dir() {
+        return Err(io::Error::new(io::ErrorKind::NotFound, "Folder not found"));
+    }
+
+    // Determine the folder name to use for renaming
+    let folder_name = folder_path
+        .file_name()
+        .and_then(|name| name.to_str())
+        .ok_or_else(|| io::Error::new(io::ErrorKind::InvalidInput, "Invalid folder name"))?;
+
+    let new_file_name = format!("{}.jpg", folder_name);
+    let new_path = folder_path.join(new_file_name);
+    if new_path.exists() {
+        if let Err(e) = fs::remove_file(&new_path) {
+            if e.kind() != std::io::ErrorKind::NotFound {
+                println!("Failed to remove old file {}: {}", new_path.display(), e);
+            }
+        } else {
+            println!("Removed old file: {}", new_path.display());
+        }
+    }
+
+    // Iterate over files in the folder
+    for entry in fs::read_dir(folder_path)? {
+        let entry = entry?;
+        let path = entry.path();
+
+        // Process only files with `.jpg` extension
+        if path.is_file() 
+            && path.extension().map_or(false, |ext| ext == "jpg") 
+            && path.file_name() != new_path.file_name()
+        {
+            // Copy and rename the file
+            fs::copy(&path, &new_path)?;
+            println!(
+                "Copied and renamed: {} -> {}",
+                path.display(),
+                new_path.display()
+            );
+        }
+    }
+
+    Ok(())
+}
